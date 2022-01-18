b35404e475b932f478d3feb0022c7873ce81822e
yasu hello <helloiamyasu@gmail.com>
2022-01-18 18:55:43 +0900

    master

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, Response, json
 application = Flask(__name__)
 
-@application.route("/ojagggyo/streamlit/main.py")
-def index():
-#    return "aplication working!"
-    return Response(
+# @application.route("/ojagggyo/streamlit/main.py")
+# def index():
+# #    return "aplication working!"
+#     return Response(
+#         "%s(%s);" %("callback", json.dumps({"name":"hogehoge"} )),
+#         mimetype="text/javascript"
+#         )
+
+# #if __name__ == "__main__":
+# #    application.run()
+
+
+
+Response(
         "%s(%s);" %("callback", json.dumps({"name":"hogehoge"} )),
         mimetype="text/javascript"
         )
-
-#if __name__ == "__main__":
-#    application.run()
