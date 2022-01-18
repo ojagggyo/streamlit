a47aaac36152b1da41a2799db29e7db0d4aacf95
yasu hello <helloiamyasu@gmail.com>
2022-01-18 18:21:58 +0900

    master

--- main.py
+++ main.py
@@ -1,55 +1,13 @@
-#import streamlit as st
-# #st.write("Good morning")
-# #st.write("Hello!")
+from flask import Flask, Response, json
+application = Flask(__name__)
 
-# import urllib.request
-# #with urllib.request.urlopen('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv') as u:
-# with urllib.request.urlopen('https://api.upbit.com/v1/ticker?markets=KRW-STEEM') as u:
-#     st.write(u.info())
-#     s = u.read()
-#     st.write(s)
- #st.write("<br>".join(s.decode('utf-8').split('\r\n')))
+@application.route("/")
+def index():
+#    return "aplication working!"
+    return Response(
+        "%s(%s);" %("callback", json.dumps({"name":"hogehoge"} )),
+        mimetype="text/javascript"
+        )
 
-
-#st.write(os.getcwd())
-
-#f = open('data.csv', 'r', encoding='UTF-8')
-#data = f.read()
-#st.write(data)
-#f.close()
-
-#st.write("callback( { \"name\" : \"Fukumori\" } )");
-
-
- 
-#出力
-#print ("Content-Type:text/javascript")
-#print ("Content-type: application/x-javascript")
-#print ()
-#print ("callback({'name':'abc'});")
-
-# import requests
-# import json
-
-# #print ("Content-Type:text/javascript")
-# print ("Content-Type:application/javascript")
-# print ("")
-# print ("callback({\"name\":\"This is JSONP\"});")
-
-# data = {
-#             'name': 'shobon',
-#             'url': 'shobon.com'
-#         }
-
-# #print ('Content-Type: application/json')
-# print ('Content-Type: application/javascript')
-# #print ('%s(%s)' % ('callback', json.dumps(data, ensure_ascii=False)) )
-# print ("callback({\"name\":\"This is JSONP\"});")
-
-from flask import Flask, jsonify
- 
-app = Flask(__name__)
- 
-@app.route('/')
-def hello_world():
-    return jsonify({'message': 'Hello world'})
+#if __name__ == "__main__":
+#    application.run()
