128c95c1a9ff8bfe3dae056ba9920d59f1b01275
yasu hello <helloiamyasu@gmail.com>
2022-01-18 12:04:52 +0900

    master

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ import urllib.request
 with urllib.request.urlopen('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv') as u:
   st.write(u.info())
   s = u.read()
-st.write(s.decode('utf-8').split('\r\n').join('<br>'))
+st.write("<br>".join(s.decode('utf-8').split('\r\n')))
