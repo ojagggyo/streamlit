b66194f896e3d1d76731b9c14ec07f8e23352f40
yasu hello <helloiamyasu@gmail.com>
2022-01-18 12:33:04 +0900

    master

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import streamlit as st
-st.write("Good morning")
-st.write("Hello!")
+#st.write("Good morning")
+#st.write("Hello!")
 
-import os
-st.write(os.getcwd())
+import urllib.request
+with urllib.request.urlopen('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv') as u:
+ st.write(u.info())
+ s = u.read()
+ st.write(s)
+ #st.write("<br>".join(s.decode('utf-8').split('\r\n')))
 
-f = open('data.csv', 'r', encoding='UTF-8')
-data = f.read()
-st.write(data)
-f.close()
 
+#st.write(os.getcwd())
 
-#import urllib.request
-#with urllib.request.urlopen('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv') as u:
-#  st.write(u.info())
-#  s = u.read()
-#  st.write("<br>".join(s.decode('utf-8').split('\r\n')))
+#f = open('data.csv', 'r', encoding='UTF-8')
+#data = f.read()
+#st.write(data)
+#f.close()
+
