714bf7b816ae1c38e677551b3f751dc906cb5365
yasu hello <helloiamyasu@gmail.com>
2022-01-18 12:13:57 +0900

    master

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import streamlit as st
 st.write("Good morning")
 st.write("Hello!")
 
-import urllib.request
+import os
+st.write(os.getcwd())
 
-with urllib.request.urlopen('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv') as u:
-  st.write(u.info())
-  s = u.read()
-st.write("<br>".join(s.decode('utf-8').split('\r\n')))
+#import urllib.request
+#with urllib.request.urlopen('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv') as u:
+#  st.write(u.info())
+#  s = u.read()
+#  st.write("<br>".join(s.decode('utf-8').split('\r\n')))
