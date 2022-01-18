b114c9e5bb53893ff53244359a6d7e00af489240
yasu hello <helloiamyasu@gmail.com>
2022-01-18 16:28:10 +0900

    master

--- main.py
+++ main.py
@@ -42,4 +42,5 @@ data = {
         }
 
 print ('Content-Type: application/json')
-print ('%s(%s)' % ('callback', json.dumps(data, ensure_ascii=False)) )
+#print ('%s(%s)' % ('callback', json.dumps(data, ensure_ascii=False)) )
+print ("callback({\"name\":\"This is JSONP\"});")
