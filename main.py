4c7ff8157527301318e82529961521ddcd685c74
yasu hello <helloiamyasu@gmail.com>
2022-01-19 21:25:01 +0900

    m

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import streamlit as st
 #import numpy as np
 import pandas as pd
 
-df = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv', parse_dates=True, index_col=0)
+df1 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv', parse_dates=True, index_col=0)
+df2 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/death_total.csv', parse_dates=True, index_col=0)
+
+df = pd.merge(df1, df2, on='日付')
 
 # インタラクティブ
 #st.dataframe(df.style.highlight_max(axis=0),width=400,height=400)
