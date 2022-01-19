4289c500a8d99a345692ad6b34d663c4bbb385cb
yasu hello <helloiamyasu@gmail.com>
2022-01-20 03:10:14 +0900

    m

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
+from tkinter.tix import COLUMN
 import streamlit as st
 import numpy as np
 import pandas as pd
 
-df1 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv', parse_dates=True, index_col=0)
-df2 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/death_total.csv', parse_dates=True, index_col=0)
+df1 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv', parse_dates=True, header=0, index_col=0)
+df2 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/death_total.csv', parse_dates=True, header=0, index_col=0)
 
+for i, name in reversed(list(enumerate(df2["死亡者数"].to_list()))):
+	if i < 1 : break
+	df2["死亡者数"][i] = df2["死亡者数"][i] - df2["死亡者数"][i-1]   
 
+st.area_chart(df1)
+st.area_chart(df2)
 
 df = pd.merge(df1, df2, on='日付')
 
-df.to_csv("merged.csv", index=False)
 
-# インタラクティブ
-#st.dataframe(df.style.highlight_max(axis=0),width=400,height=400)
-
-# 静的
-# st.table(df.style.highlight_max(axis=0))
-
-
-
-#st.line_chart(df)
-st.area_chart(df1)
-
-st.area_chart(df2)
-
-df['aaa'] = list(pd.Series(df.index,df.index-1).apply(lambda x,y: y - x))
-
-#st.bar_chart(df)
+st.dataframe(df1,width=400,height=400)
+st.dataframe(df2,width=400,height=400)
 st.dataframe(df,width=400,height=400)
