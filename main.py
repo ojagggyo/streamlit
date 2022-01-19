39d40362820d5a2ed8a2ae864d87f3fd0921b0a3
yasu hello <helloiamyasu@gmail.com>
2022-01-20 03:12:16 +0900

    m

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from tkinter.tix import COLUMN
 import streamlit as st
-import numpy as np
+#simport numpy as np
 import pandas as pd
 
 df1 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv', parse_dates=True, header=0, index_col=0)
