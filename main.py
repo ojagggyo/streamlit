c61ed5b7b827a4453de05f68e74782c011075160
yasu hello <helloiamyasu@gmail.com>
2022-01-20 03:54:34 +0900

    m

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-df1 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv', parse_dates=True, header=0, index_col=0)
-st.bar_chart(df1,use_container_width=True)
+df = pd.read_csv(\
+	'https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv'\
+	, parse_dates=True\
+	, header=0\
+	, index_col=0)
+
+st.bar_chart(df)
