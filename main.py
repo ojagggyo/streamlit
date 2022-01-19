09107d016dd668e8853ca65cdfc7c570f6d742b2
yasu hello <helloiamyasu@gmail.com>
2022-01-19 20:48:09 +0900

    m

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import streamlit as st
-import numpy as np
+#import numpy as np
 import pandas as pd
 
 df = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv')
-# df = pd.DataFrame({
-# 	'1列目':[1,2,3,4],
-# 	'2列目':[10,20,30,40],
-# })
 
-#df
 
-st.dataframe(df)
+st.dataframe(df,width=400,height=400)
