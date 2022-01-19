a30bfd57dbb924894f5dd43b47e7538492f35f89
yasu hello <helloiamyasu@gmail.com>
2022-01-19 20:25:36 +0900

    m

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ import pandas as pd
 import requests
 
 class MyDecoder(json.JSONDecoder):
-   ...  # (省略)
+    def decode(self, s):
+        result = super().decode(s)
+        return self._decode(result)
+
+    def _decode(self, o):
+        if isinstance(o, str):            
+            try:
+                if '.' in o:
+                    return float(o)
+                return int(o)
+            except ValueError:
+                return o
+        elif isinstance(o, dict):
+            return {k: self._decode(v) for k, v in o.items()}
+        elif isinstance(o, list):
+            return [self._decode(v) for v in o]
+        else:
+            return o
+
 
 @st.cache
 def load_data():
