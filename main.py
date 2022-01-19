9e67fd04d19667dcb63ed5af4da7de20ab61856d
yasu hello <helloiamyasu@gmail.com>
2022-01-19 20:33:10 +0900

    m

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
-import json
-import streamlit as st
+import numpy as np
 import pandas as pd
-import requests
 
-class MyDecoder(json.JSONDecoder):
-    def decode(self, s):
-        result = super().decode(s)
-        return self._decode(result)
+data_02 = pd.read_csv('東京23区推移0409_2月.csv')
+data_03 = pd.read_csv('東京23区推移0409_3月.csv')
+data_04 = pd.read_csv('東京23区推移0409_4月.csv')
 
-    def _decode(self, o):
-        if isinstance(o, str):            
-            try:
-                if '.' in o:
-                    return float(o)
-                return int(o)
-            except ValueError:
-                return o
-        elif isinstance(o, dict):
-            return {k: self._decode(v) for k, v in o.items()}
-        elif isinstance(o, list):
-            return [self._decode(v) for v in o]
-        else:
-            return o
+data_all = pd.concat([data_02, data_03.iloc[:, 2:], data_04.iloc[:, 2:]], axis=1)
 
-
-@st.cache
-def load_data():
-    response = requests.get("https://ckan.pf-sapporo.jp/api/action/datastore_search?resource_id=b83606f6-3aa2-4e0c-8a1a-509dd36be2ae&limit=300", verify=False)
-    response_json = MyDecoder().decode(response.text)
-    df = pd.json_normalize(response_json, record_path=["result", "records"])
-    df["日付"] = df["日付"].map(lambda x: re.search(r'\d{4}-\d{2}-\d{2}', x).group())
-    return df
-
-
-df = load_data().copy() \
-        .set_index("日付") \
-        .drop(columns="_id")
-st.write(df)
-st.line_chart(df)
+data_all.head()
