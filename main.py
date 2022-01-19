2aa2e427b2718b9bfbeddbe3767e0bdbccdc09f9
yasu hello <helloiamyasu@gmail.com>
2022-01-19 21:07:22 +0900

    m

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 #import numpy as np
 import pandas as pd
 
-df = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv')
+df = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv',header=0)
 
 # インタラクティブ
 #st.dataframe(df.style.highlight_max(axis=0),width=400,height=400)
