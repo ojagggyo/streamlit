17c8fa827907279ec3bb310a95d6ec890bfe8ec4
yasu hello <helloiamyasu@gmail.com>
2022-01-19 22:27:49 +0900

    m

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import numpy as np
+import numpy as np
 import pandas as pd
 
 df1 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv', parse_dates=True, index_col=0)
@@ -7,7 +7,7 @@ df2 = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/death_total.
 
 
 
-df = pd.merge(df1, df2, on='日付',)
+df = pd.merge(df1, df2, on='日付')
 
 df.to_csv("merged.csv", index=False)
 
@@ -24,4 +24,4 @@ st.area_chart(df1)
 
 st.area_chart(df2)
 
-st.bar_chart(df)
+st.bar_chart(df.style.precision)
