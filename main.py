bcaa21764207d116d01667306b311f9d6a7f4adb
yasu hello <helloiamyasu@gmail.com>
2022-01-19 20:51:25 +0900

    m

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 df = pd.read_csv('https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv')
 
+#st.dataframe(df.style.highlight_max(axis=0),width=400,height=400)
 
-st.dataframe(df.style.highlight_max(axis=0),width=400,height=400)
-
+st.tabls(df.style.highlight_max(axis=0))
