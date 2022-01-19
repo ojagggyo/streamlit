9b97a11501ee7738fe6a3721c8a2fd178436e37f
yasu hello <helloiamyasu@gmail.com>
2022-01-19 17:10:58 +0900

    a

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ st.write('Streamlit is cool.')
 st.text('Streamlit is cool.')
 st.markdown('Streamlit is **_really_ cool**.')
 stc.html("<p style='color:red;'> Streamlit is Awesome")
-
 stc.html("<script>alert('Hello!');</script>")
