0717dd58637564d74df51e9bc7d24aa996c2781d
yasu hello <helloiamyasu@gmail.com>
2022-01-20 11:54:12 +0900

    m

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 df = pd.read_csv(\
 	'https://toyokeizai.net/sp/visual/tko/covid19/csv/pcr_positive_daily.csv'\
 	, parse_dates=True\
-	, header=0\
 	, index_col=0)
 
 st.bar_chart(df)
